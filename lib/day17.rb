ffbb98b5403baeddf0bb8de75c6cfbe73b18c31b
Darcy Luo <darcyluojt@gmail.com>
2024-12-19 09:38:48 +0000

    pushing solution for day 17

--- lib/day17.rb
+++ lib/day17.rb
@@ -15,7 +15,8 @@ def run_programme(programme,a,b,c)
     end
     case opcode
     when 0 # combo
-      a = a / (2**combo_operand)
+      a >>= combo_operand
+      # a = a / 2**combo_operand
     when 1 # literal
       b ^= operand
     when 2 # combo
@@ -30,9 +31,9 @@ def run_programme(programme,a,b,c)
     when 5 # combo
       output << combo_operand % 8
     when 6 # combo
-      b = a / 2**combo_operand
+      b = a >> combo_operand
     when 7 # combo
-      c = a / 2**combo_operand
+      c = a >> combo_operand
     end
     pointer += 2
   end
@@ -50,15 +51,39 @@ start = Time.now
 
 programme = [2,4,1,5,7,5,0,3,4,0,1,6,5,5,3,0]
 # programme = [0,3,5,4,3,0]
-for i in 105553116266496..105553116266496*5
-  a = i
+n_last_found = 0
+# for i in 64980670000000..64980670000000 * 2
+  a = 6498067000000
   b = 0
   c = 0
-  p "Trying with a = #{a}"
-  break if run_programme(programme,a,b,c) == programme
-end
+  p run_programme(programme,a,b,c)
+
+  # # compute how many outputs match the programme:
+  # n_equal = 0
+  # p.each_with_index do |output, index|
+  #   if output == programme[index]
+  #     n_equal += 1
+  #   else
+  #     break
+  #   end
+  # end
+
+  # if [2,4] == p
+  #   puts "Found a match with a = #{a} and output = #{p}"
+  # end
+
+  # if n_equal > n_last_found
+  #   n_last_found = n_equal
+  #   a_binary_string = a.to_s(2)
+  #   # 0 pad the binary string
+  #   a_binary_string = "0" * (32 - a_binary_string.size) + a_binary_string
+  #   puts "Found a better match with a = #{a_binary_string} = #{a} and output = #{p}"
+  # end
+
 ending = Time.now
 puts "Time elapsed #{ending - start} seconds"
+
+# puts run_programme(programme,46187030,0,0).join(',')
 
 # potential_a.each do |a|
 #   b = 0
